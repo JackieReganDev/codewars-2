c485676b349583dd0dc72a50f188dbb7fe74f0b0
jackie <jackieregandev@yahoo.com>
2021-08-02 10:10:41 -0400

    remove string spaces 8kyu

---
+++ remove-string-spaces/main.js
@@ -0,0 +1,17 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+//  regex i
+function noSpace(x){
+    return x.replace(/ /g, '')
+  }
+
+  function noSpace(x){
+    return x.replace(/\s/g, '')
+   }
+
+
+//    split join methods
+
+function noSpace(x){
+    return x.split(' ').join('');
+  }
