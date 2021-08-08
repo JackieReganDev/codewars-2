34135f3841240416aaf3d530d1584941285af4d6
jackie <jackieregandev@yahoo.com>
2021-08-08 09:45:46 -0400

    palindrome 8kyu

---
+++ palindrome/main.js
@@ -0,0 +1,13 @@
+// Write function that checks if a given string (case insensitive) is a palindrome.
+
+
+function isPalindrome(x) {
+  if(x.toLowerCase() === x.toLowerCase().split('').reverse().join('')){
+    return true
+  }else{
+    return false
+  }
+  
+}
+
+const isPalindrome = x => x.toLowerCase() === x.toLowerCase().split('').reverse().join('') 
