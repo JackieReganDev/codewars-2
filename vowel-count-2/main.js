765aab8bab75dd97d38aec22b6ed786d9e3dd946
jackie <jackieregandev@yahoo.com>
2021-09-18 20:19:11 -0400

    vowel count 7kyu match method regex

---
+++ vowel-count-2/main.js
@@ -0,0 +1,23 @@
+Description:
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+
+
+
+
+function getCount(str) {
+    let vowels = ['a','e','i','o','u'];
+    return str.split('').filter(letter => {
+      return vowels.includes(letter)? true : false;
+    }).length;
+  }
+
+//   match method and regex solution
+
+  function getCount(str) {
+    return (str.match(/[aeiou]/g) || []).length
+  }
