fbf708b9ac65c6b19dd680341801cf4ca365bf62
jackie <jackieregandev@yahoo.com>
2021-09-21 11:00:50 -0400

    are arrow functions odd 8kyu

---
+++ arrow-func-odd/main.js
@@ -0,0 +1,14 @@
+// Time to test your basic knowledge in functions! Return the odds from a list:
+
+// [1, 2, 3, 4, 5]  -->  [1, 3, 5]
+// [2, 4, 6]        -->  []
+
+
+function odds(values){
+    let valuesArr = []
+    for(let i = 0; i < values.length; i++){
+      if(values[i] % 2){
+        valuesArr.push(values[i])
+      }
+    }return valuesArr
+  }
